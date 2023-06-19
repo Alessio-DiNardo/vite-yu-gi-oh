89554a13f02b4a4a56c7f3b9566d58d36b8ddf28
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-19 16:44:49 +0200

    test for components

---
+++ src/components/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>AppMain</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
