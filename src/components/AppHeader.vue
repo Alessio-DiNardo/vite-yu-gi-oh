3dcddd32f2102554bd48b96a76dc6ce1d720c0f2
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-06-19 16:41:51 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>AppHeader</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
